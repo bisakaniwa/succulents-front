4d32781f7c22127408da6f75dcf65da0eff31315
Bianca Sakaniwa <bih_sayuri@hotmail.com>
2022-11-28 10:40:46 -0300

    individual page setted with profile info

--- src/app/succulent/succulent.component.ts
+++ src/app/succulent/succulent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Succulent } from '../interface/succulent.interface';
 
 @Component({
   selector: 'app-succulent',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./succulent.component.scss']
 })
 export class SucculentComponent implements OnInit {
+
+  succulent: Succulent = {} as Succulent
 
   constructor() { }
 
